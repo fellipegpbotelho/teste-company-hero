b05bc5a7cd5b4352da0dab3b556bc85354bbabfa
Fellipe Botelho <fellipegpbotelho@gmail.com>
2021-03-11 08:45:53 -0300

    Create Company model

---
+++ src/companies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    cnpj = models.CharField(max_length=18)
+
+    def __str__(self):
+        return f"[{self.id}] {self.name}"
