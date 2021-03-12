40ee067183646f15e1f3158b04861141a91b99c7
Fellipe Botelho <fellipegpbotelho@gmail.com>
2021-03-12 18:03:35 -0300

    Improve tests for create user endpoint

--- src/api/tests/endpoints/test_create_company_user_endpoint.py
+++ src/api/tests/endpoints/test_create_company_user_endpoint.py
@@ -12,16 +12,16 @@ class CreateCompanyUserEndpointTestCase(APITestCase):
         self.company = models_company.Company.objects.create(
             name="Company 01",
             email="company01@email.com",
-            cnpj="123"
+            cnpj="36.033.000/0001-35"
         )
         kwargs = {"company_id": self.company.pk}
         self.endpoint = reverse("api:company.user.create", kwargs=kwargs)
         self.data = {
             "name": "User 01",
-            "email": "user@email.com",
+            "email": "user01@email.com",
         }
 
-    def test_create_new_company_user(self) -> None:
+    def test_create_new_user(self) -> None:
         self.client.post(path=self.endpoint, data=self.data)
         user_was_created = models_user.User.objects \
             .filter(email=self.data.get("email")) \
@@ -30,7 +30,9 @@ class CreateCompanyUserEndpointTestCase(APITestCase):
 
     def test_relates_user_with_company(self) -> None:
         self.client.post(path=self.endpoint, data=self.data)
-        user_created = models_user.User.objects.first()
+        user_created = models_user.User.objects.get(
+            email=self.data.get("email"),
+        )
         self.assertEqual(user_created.company_set.first(), self.company)
 
     def test_return_status_code_200(self) -> None:
@@ -61,11 +63,10 @@ class CreateCompanyUserEndpointTestCase(APITestCase):
         error = response.json()
         self.assertListEqual(error, ["Company not found."])
 
-    def test_do_not_create_user_if_already_exist(self):
+    def test_do_not_create_a_new_user_if_already_exist(self):
         user = models_user.User.objects.create(**self.data)
         self.company.users.add(user)
         self.company.save()
         self.client.post(path=self.endpoint, data=self.data)
         users_quantity = models_user.User.objects.count()
         self.assertEqual(users_quantity, 1)
-        self.assertEqual(self.company.users.count(), 1)
