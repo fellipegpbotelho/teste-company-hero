4fb2065330c61f5b5ca7934af2190b4aef47711b
Fellipe Botelho <fellipegpbotelho@gmail.com>
2021-03-12 17:57:27 -0300

    Create user endpoint

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from .views.company_views import CreateCompanyView
+from .views.user_views import CreateCompanyUserView
 
 app_name = "api"
 
 urlpatterns = [
-    path("companies/", CreateCompanyView.as_view(), name="company.create")
+    path("companies/", CreateCompanyView.as_view(), name="company.create"),
+    path(
+        "companies/<int:company_id>/users/",
+        CreateCompanyUserView.as_view(),
+        name="company.user.create",
+    )
 ]
--- src/users/models.py
+++ src/users/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-from users.models import User
 
-
-class Company(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
-    cnpj = models.CharField(max_length=18, unique=True)
-    users = models.ManyToManyField(User)
 
     def __str__(self):
         return f"[{self.id}] {self.name}"
