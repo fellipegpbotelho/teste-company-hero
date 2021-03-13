86b52e1411ea4d396734c75b351cf1e5b33c3dcf
Fellipe Botelho <fellipegpbotelho@gmail.com>
2021-03-12 21:39:33 -0300

    Redirect / to /api/companies/

--- src/hero/urls.py
+++ src/hero/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="api/companies/", permanent=True)),
     path("api/", include("api.urls"), name="api"),
     path("api-auth/", include("rest_framework.urls")),
 ]
