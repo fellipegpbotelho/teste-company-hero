69ac556b3b3c9d467395531ceff5c00ac06e4ef7
Fellipe Botelho <fellipegpbotelho@gmail.com>
2021-03-12 21:21:30 -0300

    Create user info endpoint

--- src/api/views/user_views.py
+++ src/api/views/user_views.py
@@ -1,19 +1,36 @@
-from rest_framework import generics
+from rest_framework import exceptions, generics
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api.serializers.user_serializers import CreateUserSerializer
+from api.serializers import user_serializers
+from users import models
 
 
 class CreateCompanyUserView(generics.CreateAPIView):
-    serializer_class = CreateUserSerializer
+    serializer_class = user_serializers.CreateUserSerializer
 
     def post(self, request: Request, id: int) -> Response:
         data = request.data
-        serializer = CreateUserSerializer(
+        serializer = user_serializers.CreateUserSerializer(
             data=data,
             context={"company_id": id},
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class GetUserView(generics.RetrieveAPIView):
+    serializer_class = user_serializers.GetUserAndCompanies
+
+    def get(self, request: Request) -> Response:
+        try:
+            email = request.GET.get("email", "")
+            user = models.User.objects.get(email=email)
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+        except models.User.DoesNotExist:
+            raise exceptions.ValidationError(
+                detail="User not found.",
+                code="400",
+            )
